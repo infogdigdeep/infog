f6f7264ca6c3bc6831e245da9ccc7ee6a803c9a9
unknown <waifu.shum@CW0022.conceptwave.com>
2012-03-29 10:05:13 -0400

    Application Context aware

--- src/main/java/com/infog/common/service/internal/StaticBeanBuilder.java
+++ src/main/java/com/infog/common/service/internal/StaticBeanBuilder.java
@@ -2,13 +2,17 @@ package com.infog.common.service.internal;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-public class StaticBeanBuilder {
+public class StaticBeanBuilder implements ApplicationContextAware {
 
+	private ApplicationContext currentCxt;
+	
 	public void registerSingleton (String name, Object beanClassInstance, ApplicationContext appCxt) {
 		GenericApplicationContext staticCxt = new StaticApplicationContext();
 		staticCxt.getBeanFactory().registerSingleton(name, beanClassInstance);
@@ -18,5 +22,15 @@ public class StaticBeanBuilder {
 	
 	public ApplicationContext getWebApplicationContext(HttpSession session) {
 		return WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+	}
+
+	public ApplicationContext getCurrentContext() {
+		return currentCxt;
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		currentCxt = context;
 	}
 }
