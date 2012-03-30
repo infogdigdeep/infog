ee4643dcca5984704e93a6dcefab0eda20341144
unknown <waifu.shum@CW0022.conceptwave.com>
2012-03-30 14:12:28 -0400

    web xml change

--- src/main/java/com/digdeep/infog/beans/controller/Content.java
+++ src/main/java/com/digdeep/infog/beans/controller/Content.java
@@ -1,12 +1,27 @@
 package com.digdeep.infog.beans.controller;
 
+import javax.enterprise.inject.Model;
 import javax.faces.bean.RequestScoped;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
+
+import org.primefaces.event.SelectEvent;
+
+import com.digdeep.infog.model.ContentInfo;
+import com.digdeep.infog.model.ContentType;
 
 @RequestScoped
+@Model
 public class Content {
-	
-	
+
+	@Inject
+	private ContentInfo currentInfo;
+
+	public ContentType[] getContentTypeValues() {
+		return ContentType.values();
+	}
+
 	public void save() {
-		
+
 	}
 }
